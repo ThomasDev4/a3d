47b0cfb654c696be86f2029335b5cb06696677e2
ThomasDev4 <tl.lemallier@gmail.com>
2025-04-07 12:42:31 +0200

    fix images

--- src/components/contactButton/ContactButton.tsx
+++ src/components/contactButton/ContactButton.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 import styles from "./ContactButton.module.css";
 
 export default function ContactButton() {
-  return (
-    <Link to="/contact" className={styles.contactButton}>
-      <img src="src/assets/images/formulaire-de-contact.png" alt="" />
-    </Link>
-  );
+	return (
+		<Link to="/contact" className={styles.contactButton}>
+			<img src="/images/formulaire-de-contact.png" alt="" />
+		</Link>
+	);
 }
