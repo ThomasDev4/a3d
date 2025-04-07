1eae78319e9ed08734d1d7344ec94a9c8b8ddfd3
ThomasDev4 <tl.lemallier@gmail.com>
2025-04-07 12:24:13 +0200

    for images

--- src/pages/servicepage/ServicePage.tsx
+++ src/pages/servicepage/ServicePage.tsx
@@ -1,43 +1,51 @@
 import styles from "./servicepage.module.css";
+import abeilleImg from "../../assets/images/abeilles.jpg";
+import frelonImg from "../../assets/images/frelons.jpg";
+import puceImg from "../../assets/images/puces.jpg";
+import cafardImg from "../../assets/images/cafards.jpg";
+import sourisImg from "../../assets/images/souris.jpg";
+import ratImg from "../../assets/images/rats.jpg";
+
 
 const services = [
   {
     id: 1,
     title: "Abeille",
-    image_path: "src/assets/images/abeilles.jpg",
+    image_path: abeilleImg,
     texte: "Résumé de l'article 1",
   },
   {
     id: 2,
     title: "Frelons",
-    image_path: "src/assets/images/frelons.jpg",
+    image_path: frelonImg,
     texte: "Résumé de l'article 2",
   },
   {
     id: 3,
     title: "Puces",
-    image_path: "src/assets/images/puces.jpg",
+    image_path: puceImg,
     texte: "Résumé de l'article 3",
   },
   {
     id: 4,
     title: "Cafard",
-    image_path: "src/assets/images/cafards.jpg",
+    image_path: cafardImg,
     texte: "Résumé de l'article 4",
   },
   {
     id: 5,
     title: "Souris",
-    image_path: "src/assets/images/souris.jpg",
+    image_path: sourisImg,
     texte: "Résumé de l'article 5",
   },
   {
     id: 6,
     title: "Rat",
-    image_path: "src/assets/images/rats.jpg",
+    image_path: ratImg,
     texte: "Résumé de l'article 6",
   },
 ];
+
 
 export default function Services() {
   return (
